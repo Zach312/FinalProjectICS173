7e88cfef43b6a2dad57352ccdf92d2785b9eb7e5
Zach312 <96224167+Zach312@users.noreply.github.com>
2021-12-15 23:44:47 -1000

    Update read.py

--- read.py
+++ read.py
@@ -1,3 +1,8 @@
 Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> print("Hello")
+>>> import pandas as pd
+pd.plotting.register_matplotlib_converters()
+import matplotlib.pyplot as plt
+%matplotlib inline
+import seaborn as sns
+print("Setup Complete")
