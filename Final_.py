c90903e016edcc33f6c63fb1c05ead933b477a9d
Zach312 <96224167+Zach312@users.noreply.github.com>
2021-12-17 02:58:55 -1000

    Update Final_.py

--- Final_.py
+++ Final_.py
@@ -5,6 +5,7 @@ import seaborn as sns
 
 Food_Insecurity_filepath = "Maui_Food_Insecurity_Index.csv"
 Food_Insecurity_data = pd.read_csv(Food_Insecurity_filepath, index_col=("County"), parse_dates=(True))
+print(Food_Insecurity_data)
 plt.figure(figsize=(10,10))
 plt.title("Food Insecurity")
 sns.barplot(x=Food_Insecurity_data.index, y=Food_Insecurity_data['Index value'])
